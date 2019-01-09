564bc72a7be0ac004323cb624cafd62f2d0075e8
joshua-disney <joshua.a.disney@gmail.com>
2019-01-09 14:51:06 -0700

    Added initial data block.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  const toDoData = [
+    { taskName: "Clean garage" },
+    { taskName: "Wash Dishes" },
+    { taskName: "Vaccuum rug" },
+    { taskName: "Cook dinner" },
+    { taskName: "Play with Lynette" }
+  ];
+
   render() {
     return (
       <div>
